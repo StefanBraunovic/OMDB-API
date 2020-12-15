11f1c36475552c622ef3fd13b45920b98033d5ed
Stefan Braunovic <stefan.braunovic55@gmail.com>
2020-12-15 15:15:07 +0100

    AJAX JQUERY

--- app.js
+++ app.js
@@ -1,1 +1,38 @@
-console.log("Hello World");
+
+$(document).ready(function(){
+    $('#btn').click(()=>{
+       
+    const newUrl = createURL();
+
+    callApi(newUrl)
+
+    })
+
+   
+})
+
+
+
+function createURL(){
+    const  title = document.querySelector('#naziv').value;
+    const url ="http://www.omdbapi.com/?apikey=cb417685&t="+title;
+    
+console.log(url);
+return url;
+}
+
+function callApi(url){
+    $.ajax({
+        type:"GET",
+        url:url,
+        success: function(response){
+            console.log(response);
+        }
+        
+    })
+    
+}
+
+
+
+
